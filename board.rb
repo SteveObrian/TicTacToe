ea5b8447cc8ecd8aec1a6833a1294f3ec4698521
SteveObrian <steve.krasel@posteo.de>
2018-03-05 16:06:31 +0100

    Initial commit

---
+++ board.rb
@@ -0,0 +1,50 @@
+class Board
+  WIDTH = 3
+  HEIGHT = WIDTH
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new(HEIGHT) { Array.new(WIDTH,:" ") }
+  end
+
+  def print_grid
+    @grid.each do |row|
+      puts
+      row.each do |cell|
+        print "[#{cell}]"
+      end
+    end
+    puts
+  end
+
+  def row_win?(marker)
+    @grid.any? do |row|
+      row.all? do |cell|
+        cell == marker
+      end
+    end
+  end
+
+  def column_win?
+
+  end
+
+  def diagonal_win?
+
+  end
+end
+def print_and_check
+  @b.print_grid
+  p @b.row_win?(:O)
+  p @b.row_win?(:X)
+end
+@b = Board.new
+print_and_check
+@b.grid[0][0] = :O
+
+print_and_check
+@b.grid[0][1] = :O
+
+print_and_check
+@b.grid[0][2] = :O
+print_and_check
