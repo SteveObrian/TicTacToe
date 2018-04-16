8c8da8d28274da680a62fd3e74a30614f4071ed7
SteveObrian <steve.krasel@posteo.de>
2018-04-16 17:49:55 +0200

    Refactors session class

--- session.rb
+++ session.rb
@@ -15,9 +15,12 @@ class Session
   private
 
   def play_loop
+    puts 'Welcome to tic tac toe.'
     loop do
       puts 'Starting a new game!'
-      play_a_game
+      game = Game.new(@players)
+      @last_winner = game.winner
+      update_scores
       puts display_scores
       break unless play_again?
     end
@@ -39,29 +42,30 @@ class Session
     end
   end
 
-  def play_a_game
-    game = Game.new(@players)
-    winner = game.play_until_end
-    if winner
-      puts "#{winner.name} won!"
-      winner.increment_score
-    else
-      puts 'The game was a tie!'
-      @ties += 1
-    end
+  def update_scores
+    @last_winner ? @last_winner.increment_score : @ties += 1
+  end
+
+  def announced_winner
+    @last_winner ? "#{@last_winner.name} won!" : 'The game was a tie!'
   end
 
   def display_scores
-    scores_array = ['', "The score after #{games_played} games"]
+    scores_array = [
+      announced_winner,
+      '',
+      "The score after #{games_played} games"
+    ]
+    scores_array.concat(
+      @players.map { |player| "#{player.name} has won #{player.score} times" }
+    )
     scores_array <<
-      @players.map do |player|
-        "#{player.name} has won #{player.score} times"
-      end
-    scores_array << "#{@players[0].score} and #{@players[1].score} have tied #{@ties} times"
-    scores_array << ''
+      "#{@players[0].name} and #{@players[1].name} have tied #{@ties} times"
+    scores_array <<
+      ''
   end
 
   def games_played
-    @ties + @players.reduce(0){ |total, player| total + player.score }
+    @ties + @players.reduce(0) { |total, player| total + player.score }
   end
 end
